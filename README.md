1f13b82321a40bdc953a414e5c99659c4bbc810b
kydos <angelo@icorsaro.net>
2020-09-22 13:57:17 +0200

    initial version of ATO utils for Cyclone DDS

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cdds-blob
-This is a small C library that makes it relatively straightforward to read and write blobs of CDR
+This is a small C library that provides a series of utilities funcitons for Cycloine DDS.
+At the present time the following are available:
+
+- Utilities for reading / writing CDR data
+
---
+++ include/cdds/cdds_util.h
@@ -0,0 +1,33 @@
+#ifndef ATOLAB_CDDS_UTIL_H_
+#define ATOLAB_CDDS_UTIL_H_
+
+#include <assert.h>
+#include <limits.h>
+#include <string.h>
+
+#include "dds/ddsrt/mh3.h"
+#include "dds/ddsrt/md5.h"
+#include "dds/ddsrt/bswap.h"
+
+#include "dds/dds.h"
+#include "dds/ddsi/ddsi_serdata.h"
+#include "dds/ddsi/q_radmin.h"
+
+#include "dds/dds.h"
+#include "dds/ddsrt/atomics.h"
+#include "dds/ddsrt/process.h"
+#include "dds/ddsrt/threads.h"
+
+struct z_ddsi_payload {
+  struct ddsi_serdata sd;
+  size_t size;
+  enum ddsi_serdata_kind kind;
+  unsigned char* payload;
+};
+
+dds_entity_t z_create_blob_topic(dds_entity_t dp, char *topic_name, char* type_name, bool is_keyless);
+
+int z_take_blob(dds_entity_t rd, struct z_ddsi_payload** sample, dds_sample_info_t * si);
+
+
+#endif /* ATOLAB_CDDS_UTIL_H_ */
